2e3357f06f2ad0bf0b1308948ea6426b208f1a0f
joshuaalbert <albert@strw.leidenuniv.nl>
2020-09-24 01:28:26 +0200

    * refactor

--- jaxns/gaussian_process/tomographic_kernel/tests.py
+++ jaxns/gaussian_process/tomographic_kernel/tests.py
@@ -1,50 +1,9 @@
-from jax import random, vmap, numpy as jnp
-from jax.scipy.special import logsumexp
+from jax import numpy as jnp, random, vmap
 
-from jaxns.gaussian_process.utils import squared_norm
-
-
-def log_tomographic_weight_function_stochastic(key, u, x1, p1, x2, p2):
-    """
-    int w(x) f(x) dx = sum_i w(dx * i) f(dx * i) dx
-    where,
-    int w(x) dx = sum_i w(dx * i) dx = 1
-    Args:
-        key:
-        u:
-        x1: [N, 3]
-        p1: [N, 3]
-        x2: [M, 3]
-        p2: [M, 3]
-
-    Returns:
-        w(dx*i) dx / sum_i w(dx * i) dx
-        [N, M] shaped
-
-    """
-    n = u.size**2
-    N = x1.shape[0]
-    M = x2.shape[0]
-    t1 = random.uniform(key, shape=(n, N, 1))
-    t2 = random.uniform(key, shape=(n, M, 1))
-    #L, N, M
-    norm_squared = vmap(squared_norm) (x1 + t1 * p1, x2 + t2 * p2)
-    bins = jnp.concatenate([u, u[-1:] + u[-1] - u[-2]])
-    #N*M, U
-    hist = vmap(lambda x: jnp.histogram(x, bins)[0])(jnp.reshape(norm_squared, (n, -1)).T)
-    #N,M,U
-    hist = jnp.reshape(hist, (x1.shape[0], x2.shape[0], u.size))
-    log_hist = jnp.log(hist)
-    log_du = jnp.diff(bins)
-    log_w = log_hist + log_du
-    #N,M,U
-    log_w = log_w - logsumexp(log_w, axis=-1, keepdims=True)
-    log_w = jnp.where(hist == 0., -jnp.inf, log_w)
-    return log_w
-
-
-def tomographic_weight_function_stochastic(key, u, x1, p1, x2, p2):
-    return jnp.exp(log_tomographic_weight_function_stochastic(key, u, x1, p1, x2, p2))
+from jaxns.gaussian_process.tomographic_kernel.tomographic_kernel_utils import tomographic_weight_function_stochastic, \
+    tomographic_weight_function_outer, tomographic_weight_function, log_tomographic_weight_dimensionless_function, \
+    get_polynomial_form, cumulative_tomographic_weight_dimensionless_function, \
+    cumulative_tomographic_weight_dimensionless_polynomial
 
 
 def test_tomographic_weight_function_stochastic():
@@ -61,20 +20,6 @@ def test_tomographic_weight_function_stochastic():
     plt.show()
 
 
-def tomographic_weight_function_outer(gamma, x1, x2, p1, p2, S):
-    def inner(x1,p1):
-        #M
-        return vmap(lambda x2,p2: tomographic_weight_function(gamma, x1,x2,p1,p2,S))(x2,p2)
-    return vmap(inner)(x1,p1) #N, M
-
-
-def log_tomographic_weight_function_outer(gamma, x1, x2, p1, p2, S):
-    def inner(x1,p1):
-        #M
-        return vmap(lambda x2,p2: log_tomographic_weight_function(gamma, x1,x2,p1,p2,S))(x2,p2)
-    return vmap(inner)(x1,p1) #N, M
-
-
 def test_tomographic_weight_function_outer():
     keys = random.split(random.PRNGKey(0),6)
     x1 = random.normal(keys[0], shape=(5,2,))
@@ -86,49 +31,29 @@ def test_tomographic_weight_function_outer():
     assert tomographic_weight_function_outer(gamma,x1,x2,p1,p2,S=15).shape == (x1.shape[0],x2.shape[0],gamma.size-1)
 
 
-def tomographic_weight_function(gamma, x1, x2, p1, p2=None, S=25):
-    return jnp.exp(log_tomographic_weight_function(gamma, x1, x2, p1, p2=p2, S=S))
-
-
-def log_tomographic_weight_function(gamma, x1, x2, p1, p2=None, S=25):
-    parabolic = False
-    if p2 is None:
-        parabolic = True
-        p2 = p1
-
-    x12 = x1 - x2
-    A = p1 @ p1
-    C = p2 @ p2
-    B = -2. * p1 @ p2
-    D = 2. * x12 @ p1
-    E = -2.* x12 @ p2
-    F = x12 @ x12 - gamma
-
-    t1 = jnp.linspace(0., 1., S)[:, None]
-    H = (D**2 - 4.*A * F + 2. * B * D * t1 - 4. * A * E * t1 + B**2 * t1**2 - 4.*A * C * t1**2)
-    u = (-D - B * t1)
-    lower = jnp.clip(0.5 * (u - jnp.sqrt(H)) / A, 0., 1.)
-    upper = jnp.clip(0.5 * (u + jnp.sqrt(H)) / A, 0., 1.)
-    diff = (upper - lower)/(S-1)
-    if not parabolic:
-        reg_valid = H >= 0.
-        cdf = jnp.sum(jnp.where(reg_valid,diff, 0.), axis=0)
-    else:
-        cdf = jnp.sum(diff, axis=0)
-    return jnp.log(jnp.diff(cdf)) - jnp.log(jnp.diff(gamma))
-
-
 def test_tomographic_weight():
     import pylab as plt
     from jax import jit
 
-    @jit
+    # @jit
     def tomo_weight(gamma, x1,x2,p1,p2):
         return tomographic_weight_function(gamma, x1, x2,p1,p2,S=15)
 
     @jit
     def tomo_weight_ref(gamma, x1, x2, p1, p2):
         return tomographic_weight_function(gamma, x1, x2, p1, p2, S=150)
+
+    @jit
+    def tomo_weight_dimensionless_ref(gamma, x1, x2, p1, p2):
+        x12 = x1-x2
+        h = jnp.linalg.norm(x12)
+        n = x12/h
+        w1=p1/h
+        w2=p2/h
+        gamma_prime = gamma/h**2
+        return jnp.exp(log_tomographic_weight_dimensionless_function(gamma_prime, n,w1, w2, S=150))/h**2
+
+
 
     for i in range(10):
         keys = random.split(random.PRNGKey(i),6)
@@ -155,9 +80,14 @@ def test_tomographic_weight():
         w = tomo_weight(bins, x1, x2, p1, p2)
         plt.plot(gamma, w, label='analytic')
         w_ref = tomo_weight_ref(bins, x1, x2, p1, p2)
+        # w_ref = tomo_weight_dimensionless_ref(bins, x1,x2,p1,p2)
         plt.plot(gamma, w_ref, label='analytic ref')
         plt.legend()
         plt.show()
+        plt.plot()
+
+        # test same as dimensionless
+        # assert jnp.all(jnp.isclose(tomo_weight_dimensionless_ref(bins, x1, x2, p1, p2), w_ref))
 
 
 def test_tomographic_weight_rel_err():
@@ -206,3 +136,91 @@ def test_tomographic_weight_rel_err():
         plt.hist(rel_error,bins ='auto')
         plt.title("{} : {:.2f}|{:.2f}|{:.2f}".format(S, *jnp.percentile(rel_error, [5,50,95])))
         plt.show()
+
+
+def test_get_Q():
+    Q = get_polynomial_form()
+    import pylab as plt
+    from jax import jit
+
+    @jit
+    def tomo_weight_ref(gamma, x1, x2, p1, p2):
+        return tomographic_weight_function(gamma, x1, x2, p1, p2, S=150)
+
+    @jit
+    def cumulative_tomo_weight_function_dimensionless(gamma, x1, x2, p1, p2):
+        x12 = x1 - x2
+        h = jnp.linalg.norm(x12)
+        n = x12 / h
+        w1 = p1 / h
+        w2 = p2 / h
+        gamma_prime = gamma / h ** 2
+        return cumulative_tomographic_weight_dimensionless_function(gamma_prime, n, w1, w2, S=150)
+
+    @jit
+    def cumulative_tomo_weight_polynomial_dimensionless(gamma, x1, x2, p1, p2):
+        x12 = x1 - x2
+        h = jnp.linalg.norm(x12)
+        n = x12 / h
+        w1 = p1 / h
+        w2 = p2 / h
+        gamma_prime = gamma / h ** 2
+        return vmap(lambda gamma_prime: cumulative_tomographic_weight_dimensionless_polynomial(Q, gamma_prime, n, w1, w2))(gamma_prime)
+        # return jnp.exp(log_tomographic_weight_dimensionless_function(gamma_prime, n, w1, w2, S=150)) / h ** 2
+
+    for i in range(10):
+        keys = random.split(random.PRNGKey(i), 6)
+        x1 = jnp.concatenate([10. * random.uniform(keys[0], shape=(2,)), jnp.zeros((1,))], axis=-1)
+        p1 = jnp.concatenate(
+            [4. * jnp.pi / 180. * random.uniform(keys[1], shape=(2,), minval=-1, maxval=1),
+             jnp.ones((1,))], axis=-1)
+        p1 = 4 * p1 / jnp.linalg.norm(p1, axis=-1, keepdims=True)
+
+        x2 = jnp.concatenate([4. * random.uniform(keys[2], shape=(2,)), jnp.zeros((1,))], axis=-1)
+        p2 = jnp.concatenate(
+            [4. * jnp.pi / 180. * random.uniform(keys[3], shape=(2,), minval=-1, maxval=1),
+             jnp.ones((1,))], axis=-1)
+        p2 = 4 * p2 / jnp.linalg.norm(p2, axis=-1, keepdims=True)
+
+        t1 = random.uniform(keys[4], shape=(10000,))
+        t2 = random.uniform(keys[5], shape=(10000,))
+        u1 = x1 + t1[:, None] * p1
+        u2 = x2 + t2[:, None] * p2
+        gamma = jnp.linalg.norm(u1 - u2, axis=1) ** 2
+        plt.hist(gamma.flatten(), bins=100, density=True, label='histogram')
+        hist, bins = jnp.histogram(gamma.flatten(), density=True, bins=100)
+        gamma = 0.5 * (bins[:-1] + bins[1:])
+        w_ref = tomo_weight_ref(bins, x1, x2, p1, p2)
+        plt.plot(gamma, w_ref, label='analytic ref')
+        plt.legend()
+        plt.show()
+        cdf_ref = cumulative_tomo_weight_function_dimensionless(gamma, x1, x2, p1, p2)
+        cdf_poly = cumulative_tomo_weight_polynomial_dimensionless(gamma, x1, x2, p1, p2)
+        gamma_prime = gamma/jnp.linalg.norm(x1-x2)
+        plt.plot(gamma_prime,cdf_ref,label='ref')
+        plt.plot(gamma_prime,cdf_poly, label='poly')
+        plt.legend()
+        plt.show()
+
+
+def test_clip_algebra():
+    z = jnp.linspace(-1., 2., 10)
+    def max0(z):
+        return 0.5*(z + jnp.abs(z))
+    assert jnp.all(max0(z)==jnp.maximum(z,0.))
+
+    def min1(z):
+        #min(z,1) = min(z-1,0) + 1 = -max(1-z,0) + 1
+        # return jnp.minimum(z-1,0) + 1.# -max0(1.-z) + 1.
+        return  -max0(1.-z) + 1.
+
+    assert jnp.all(jnp.isclose(min1(z), jnp.minimum(z, 1.)))
+
+    def clip01(z):
+        # 1/2 + 1/4 * (z+abs(z))  - abs(1/2 - 1/4 * (z+abs(z)))
+        z = 0.25*z
+        z = z + jnp.abs(z)
+        return 0.5 + z - jnp.abs(0.5 - z)
+        # return min1(max0(z))
+
+    assert jnp.all(jnp.isclose(clip01(z) , jnp.clip(z, 0., 1.)))
