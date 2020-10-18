* new param tracking is working, but aggregation statistics are still off. The evidence is converging to the wrong value.

jaxns/examples/mvn_data_mvn_prior.py
@@ -31,14 +31,23 @@ def main():
 
     log_likelihood = lambda x, **kwargs: log_normal(x, data_mu, data_cov)
 
-    prior_transform = PriorChain().push(MVNDiagPrior('x', prior_mu, jnp.sqrt(jnp.diag(prior_cov))))
-    # prior_transform = LaplacePrior(prior_mu, jnp.sqrt(jnp.diag(prior_cov)))
-    # prior_transform = UniformPrior(-20.*jnp.ones(ndims), 20.*jnp.ones(ndims))
-    ns = NestedSampler(log_likelihood, prior_transform, sampler_name='multi_ellipsoid')
+
 
     def run_with_n(n):
         @jit
         def run(key):
+            prior_transform = PriorChain().push(MVNDiagPrior('x', prior_mu, jnp.sqrt(jnp.diag(prior_cov))))
+            # prior_transform = LaplacePrior(prior_mu, jnp.sqrt(jnp.diag(prior_cov)))
+            # prior_transform = UniformPrior(-20.*jnp.ones(ndims), 20.*jnp.ones(ndims))
+            def param_mean(x, **args):
+                return x
+            def param_covariance(x, **args):
+                return jnp.outer(x,x)
+
+            ns = NestedSampler(log_likelihood, prior_transform, sampler_name='multi_ellipsoid',
+                               x_mean=param_mean,
+                               x_cov=param_covariance
+                               )
             return ns(key=key,
                       num_live_points=n,
                       max_samples=1e5,
@@ -46,18 +55,20 @@ def main():
                       termination_frac=0.01,
                       stoachastic_uncertainty=False,
                       sampler_kwargs=dict(depth=3))
-        # print(make_jaxpr(run)(random.PRNGKey(0)))
+        print(len(str(make_jaxpr(run)(random.PRNGKey(0)))))
+        #old 720781
+        #new 760803
+        #new new 648404
         results = run(random.PRNGKey(0))
         return results
 
-    for n in [200]:
-        # with disable_jit():
+    for n in [500]:
         results = run_with_n(n)
-        print(results.efficiency)
+        print(results)
         plt.scatter(n, results.logZ)
         plt.errorbar(n, results.logZ, yerr=results.logZerr)
 
-    plt.hlines(true_logZ, 50, 200)
+    plt.hlines(true_logZ, 0, n)
     plt.show()
 
     # plot_samples_development(results, save_name='./example.mp4')
@@ -70,6 +81,7 @@ def main():
 
 
 if __name__ == '__main__':
+
     # import jax.profiler
     # server = jax.profiler.start_server(9999)
     # input("Ready? ")

jaxns/utils.py
@@ -376,10 +376,17 @@ def left_broadcast_mul(x, y):
     return jnp.reshape(x, (-1,) + tuple([1] * (len(y.shape) - 1))) * y
 
 def tuple_prod(t):
+    if len(t) == 0:
+        return 1
     res = t[0]
     for a in t[1:]:
         res *= a
     return res
+
+def test_tuple_prod():
+    assert tuple_prod(()) == 1
+    assert tuple_prod((1,2,3)) == 6
+    assert tuple_prod((4,)) == 4
 
 
 def msqrt(A):
@@ -428,7 +435,7 @@ def test_logaddexp():
 def signed_logaddexp(log_abs_val1, sign1, log_abs_val2, sign2):
     amax = jnp.maximum(log_abs_val1, log_abs_val2)
     signmax = jnp.where(log_abs_val1 > log_abs_val2, sign1, sign2)
-    delta = -jnp.abs(log_abs_val2 - log_abs_val1)
+    delta = -jnp.abs(log_abs_val2 - log_abs_val1)#nan iff inf - inf
     sign = sign1*sign2
     return jnp.where(jnp.isnan(delta),
                       log_abs_val1 + log_abs_val2,  # NaNs or infinities of the same sign.
@@ -493,10 +500,22 @@ def test_signed_logaddexp():
     ans_sign = jnp.sign(ans)
     log_abs_ans = jnp.log(jnp.abs(ans))
     log_abs_c, sign_c = signed_logaddexp(a, sign1, b, sign2)
-    # assert sign_c == ans_sign
+    assert sign_c == ans_sign
     assert jnp.isclose(log_abs_c, log_abs_ans)
 
     u = [0., 1.]
+    a = jnp.log(jnp.abs(u[0]))
+    b = jnp.log(jnp.abs(u[1]))
+    sign1 = jnp.sign(u[0])
+    sign2 = jnp.sign(u[1])
+    ans = u[0] + u[1]
+    ans_sign = jnp.sign(ans)
+    log_abs_ans = jnp.log(jnp.abs(ans))
+    log_abs_c, sign_c = signed_logaddexp(a, sign1, b, sign2)
+    assert sign_c == ans_sign
+    assert jnp.isclose(log_abs_c, log_abs_ans)
+
+    u = [0., -1.]
     a = jnp.log(jnp.abs(u[0]))
     b = jnp.log(jnp.abs(u[1]))
     sign1 = jnp.sign(u[0])