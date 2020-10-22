c7cd4d28a4592e553e4288a91713e1bee4196f8a
joshuaalbert <45720Physics>
2020-10-23 00:46:42 +0200

    * clean up

--- jaxns/likelihood_samplers/__init__.py
+++ jaxns/likelihood_samplers/__init__.py
@@ -1,6 +1,6 @@
 from .constrained_hmc import constrained_hmc, init_chmc_sampler_state
 from .box import expanded_box, init_box_sampler_state
-from .slice import slice_sampling, slice_sampling_poly, init_slice_sampler_state
+from .slice import slice_sampling, init_slice_sampler_state
 from .ellipsoid import ellipsoid_sampler, init_ellipsoid_sampler_state
 from .cubes import cubes, init_cubes_sampler_state
 from .simplex import simplex, init_simplex_sampler_state
