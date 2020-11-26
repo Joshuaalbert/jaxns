56bbcc08ca61323fb3d820ca1bb120023581c052
joshuaalbert <albert@strw.leidenuniv.nl>
2020-11-26 18:30:47 +0100

    * clean up

--- install_requirements.sh
+++
@@ -1,3 +0,0 @@
-pip install numpy scipy matplotlib PyQt5 vtk astropy jax jaxlib mkl tensorflow tensorflow-probability pytest
-pip install git+https://github.com/pyro-ppl/numpyro.git  -e .[dev]
-pip install mayavi
