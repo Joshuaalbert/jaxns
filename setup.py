2d77ecda44764073829bd3096cad3b2c6495337e
joshuaalbert <albert@strw.leidenuniv.nl>
2020-11-27 12:29:30 +0100

    * pip compliance

--- setup.py
+++ setup.py
@@ -3,22 +3,33 @@
 from setuptools import setup
 from setuptools import find_packages
 
-__minimum_numpy_version__ = '1.16.2'
+__maximum_numpy_version__ = '1.19.0'
 __minimum_jax_version__ = '0.1.67'
 
-setup_requires = ['numpy>=' + __minimum_numpy_version__,
+setup_requires = ['numpy<' + __maximum_numpy_version__,
                   'jax>=' + __minimum_jax_version__]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='jaxns',
       version='0.0.1',
       description='Nested Sampling in JAX',
-      author=['Joshua G. Albert'],
-      author_email=['albert@strw.leidenuniv.nl'],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/joshuaalbert/jaxns",
+      author='Joshua G. Albert',
+      author_email='albert@strw.leidenuniv.nl',
       setup_requires=setup_requires,
       tests_require=[
           'pytest>=2.8',
       ],
-      # package_data= {'born_rime':['arrays/*', 'data/*']},
       package_dir={'': './'},
-      packages=find_packages('./')
+      packages=find_packages('./'),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
       )
