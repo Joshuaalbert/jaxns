5f70bf0acb08d93819d079727b7c038e1f0ec22a
joshuaalbert <albert@strw.leidenuniv.nl>
2021-03-08 11:24:28 -0500

    * 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='jaxns',
-      version='0.0.2',
+      version='0.0.4',
       description='Nested Sampling in JAX',
       long_description=long_description,
       long_description_content_type="text/markdown",
