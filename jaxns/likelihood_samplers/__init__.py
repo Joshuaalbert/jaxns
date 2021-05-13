afa4f6f093d78fa6c65a69aeebc2cce15cd7358d
joshuaalbert <albert@strw.leidenuniv.nl>
2021-05-13 14:04:46 -0400

    * clean up

--- jaxns/likelihood_samplers/__init__.py
+++ jaxns/likelihood_samplers/__init__.py
@@ -1,7 +1,7 @@
 from .slice import slice_sampling, init_slice_sampler_state
 from .multi_ellipsoid import multi_ellipsoid_sampler, init_multi_ellipsoid_sampler_state
 from .discrete import sample_discrete_subspace, init_discrete_sampler_state
-from .multi_slice import multi_slice_sampling, init_multi_slice_sampler_state
-from .nn_crumbs import nn_crumbs_sampling, init_nn_crumbs_sampler_state
-from .cone_slice import cone_slice_sampling, init_cone_slice_sampler_state
+# from .multi_slice import multi_slice_sampling, init_multi_slice_sampler_state
+# from .nn_crumbs import nn_crumbs_sampling, init_nn_crumbs_sampler_state
+# from .cone_slice import cone_slice_sampling, init_cone_slice_sampler_state
 from .common import SamplingResults
