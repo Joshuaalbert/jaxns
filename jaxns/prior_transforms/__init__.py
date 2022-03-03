837a35c8f7492a4955654e29091513f94ef40531
joshuaalbert <albert@strw.leidenuniv.nl>
2022-03-03 16:06:04 -0500

    * Refact no-quantile priors * tutorials for no-quantile and discrete likelihood

--- jaxns/prior_transforms/__init__.py
+++ jaxns/prior_transforms/__init__.py
@@ -4,4 +4,5 @@ from jaxns.prior_transforms.identifiable import *
 from jaxns.prior_transforms.levy import *
 from jaxns.prior_transforms.mixture import *
 from jaxns.prior_transforms.discrete import *
+from jaxns.prior_transforms.no_quantile import *
 from jaxns.prior_transforms.prior_chain import PriorChain
