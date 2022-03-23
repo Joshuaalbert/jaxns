c97a42631ecd7dab6373fa11e82cf98eccdbe120
joshuaalbert <albert@strw.leidenuniv.nl>
2022-03-23 16:42:45 -0400

    * chunked pmap and more maps

--- jaxns/internals/maps.py
+++ jaxns/internals/maps.py
@@ -1,8 +1,21 @@
 import inspect
+from jax import tree_map, pmap, numpy as jnp
+from jax._src.lax.control_flow import scan
+from jax.lax import dynamic_update_slice
 import logging
 
 logger = logging.getLogger(__name__)
 
+
+def replace_index(operand, update, start_index):
+    """
+    Replaces an index or slice with an update.
+    If update is too big to respect start_index then start_index is shifted, which will give non-intuitive results.
+    """
+    if len(operand.shape) != len(update.shape):
+        update = update[None]
+    start_indices = [start_index] + [jnp.asarray(0, jnp.int_)] * (len(update.shape) - 1)
+    return dynamic_update_slice(operand, update.astype(operand.dtype), start_indices)
 
 def dict_multimap(f, d, *args):
     """
@@ -71,3 +84,52 @@ def prepare_func_args(f):
         return f(**args_with_values)
     _f.__doc__ = f.__doc__
     return _f
+
+
+def chunked_pmap(f, chunksize, *, batch_size=None):
+    def _f(*args, batch_size=batch_size, **kwargs):
+        def queue(*args, **kwargs):
+            """
+            Distributes the computation in queues which are computed with scan.
+            Args:
+                *args:
+            """
+
+            def body(state, X):
+                (args, kwargs) = X
+                return state, f(*args, **kwargs)
+            _, result = scan(body, (), (args, kwargs))
+            return result
+        if chunksize > 1:
+            if batch_size is None:
+                batch_size = args[0].shape[0] if len(args) > 0 else None
+            assert batch_size is not None, "Couldn't get batch_size, please provide explicitly"
+            remainder = batch_size % chunksize
+            extra = (chunksize - remainder) % chunksize
+            args = tree_map(lambda arg: _pad_extra(arg, chunksize), args)
+            kwargs = tree_map(lambda arg: _pad_extra(arg, chunksize), kwargs)
+            result = pmap(queue)(*args, **kwargs)
+            result = tree_map(lambda arg: jnp.reshape(arg, (-1,) + arg.shape[2:]), result)
+            if extra > 0:
+                result = tree_map(lambda x: x[:-extra], result)
+        else:
+            result = queue(*args, **kwargs)
+        return result
+    _f.__doc__ = f.__doc__
+    _f.__annotations__ = f.__annotations__
+    return _f
+
+
+def _pad_extra(arg, chunksize):
+    N = arg.shape[0]
+    remainder = N % chunksize
+    if (remainder != 0) and (N > chunksize):
+        # only pad if not a zero remainder
+        extra = (chunksize - remainder) % chunksize
+        arg = jnp.concatenate([arg] + [arg[0:1]] * extra, axis=0)
+        N = N + extra
+    else:
+        extra = 0
+    arg = jnp.reshape(arg, (chunksize, N // chunksize) + arg.shape[1:])
+    T = N // chunksize
+    return arg
