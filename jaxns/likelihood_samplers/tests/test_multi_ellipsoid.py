06997d63098f07408307d50b00c2e09e5091f79b
joshuaalbert <albert@strw.leidenuniv.nl>
2022-03-25 16:42:21 -0400

    * refactor

---
+++ jaxns/likelihood_samplers/tests/test_multi_ellipsoid.py
@@ -0,0 +1,9 @@
+from jax import numpy as jnp
+from jaxns.likelihood_samplers.multi_ellipsoid import log_ellipsoid_volume
+
+
+def test_log_ellipsoid_volume():
+    radii = jnp.ones(2)
+    assert jnp.isclose(log_ellipsoid_volume(radii), jnp.log(jnp.pi))
+    radii = jnp.ones(3)
+    assert jnp.isclose(log_ellipsoid_volume(radii), jnp.log(4. * jnp.pi / 3.))
