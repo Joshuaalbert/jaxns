fe75420308aa895491f57c7e76d0dcfb766b268c
Joshua G. Albert <albert@strw.leidenuniv.nl>
2022-06-02 17:37:18 -0400

    * bayesian optimiser module load/saving and parallel option working.

--- jaxns/modules/bayesian_optimisation/tests/test_bayesian_optimiser.py
+++ jaxns/modules/bayesian_optimisation/tests/test_bayesian_optimiser.py
@@ -19,35 +19,36 @@ def test_bayesian_optimisation():
         return -0.5 * jnp.sum(x ** 4 - 16 * x ** 2 + 5 * x)
 
     def test_example(ndim):
-        with PriorChain() as search_prior_chain:
-            UniformPrior('x', -5. * jnp.ones(ndim), 5. * jnp.ones(ndim))
-
-        bo = BayesianOptimiser(search_prior_chain)
-        U, X = bo.initialise_experiment(ndim*2)
-        Y = list(map(lambda x: objective(**x), X))
-
-        for u,x,y in zip(U, X, Y):
-            bo.add_result(u,x,y)
-
-        print(bo)
-
-        for i in range(num_steps):
-            u, x = bo.choose_next_sample_location()
-            y = objective(**x)
-            bo.add_result(u,x,y)
-            print(u, x, y)
-            print(bo)
 
         lower_bound = 39.16616 * ndim
         upper_bound = 39.16617 * ndim
+        print(f"Optimal value in ({lower_bound}, {upper_bound}).")
+
         x_max = -2.903534
-        # print(ndim, jnp.abs(results.log_L_max - 0.5 * (lower_bound + upper_bound)))
-        # print(ndim, jnp.abs(results.sample_L_max['x'] - x_max))
-        # assert jnp.isclose(results.log_L_max,
-        #                    0.5 * (lower_bound + upper_bound),
-        #                    atol=2. * (upper_bound - lower_bound))
-        # assert jnp.allclose(results.sample_L_max['x'], x_max, atol=2e-1 * ndim / 8)
-        # print(results)
+
+        print(f"Global optimum at {jnp.ones(ndim) * x_max}")
+
+        with PriorChain() as search_prior_chain:
+            UniformPrior('x', -5. * jnp.ones(ndim), 5. * jnp.ones(ndim))
+
+        bo = BayesianOptimiser(search_prior_chain, key=random.PRNGKey(6465249))
+        observations = bo.initialise_experiment(ndim * 2)
+        for obs in observations:
+            obs.set_response(objective(**obs.sample_point))
+            bo.add_result(obs)
+
+        bo.save_state("save_file.npz")
+        bo.load_state("save_file.npz")
+
+        print(bo)
+        for i in range(num_steps):
+            obs = bo.choose_next_sample_location()
+            obs.set_response(objective(**obs.sample_point))
+            bo.add_result(obs)
+            print(obs)
+            print(bo)
+
+        bo.plot_progress()
 
     test_example(2)
 
