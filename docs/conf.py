487c21793af13ed94dca3415d0a6c42c40008f20
Alex Lyttle <a.j.lyttle@outlook.com>
2022-08-23 10:12:21 +0100

    Unlimited navigation depth

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 html_theme_options = {
-    # "collapse_navigation": False,
+    "navigation_depth": -1,
 }
 
 # -- Options for AutoAPI -----------------------------------------------------
