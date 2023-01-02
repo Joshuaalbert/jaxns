4e30b144dd4fa57781a317c5eb94d14c100dea79
joshuaalbert <albert@strw.leidenuniv.nl>
2023-01-01 19:07:15 -0500

    * DI of slice sampler

--- jaxns/adaptive_refinement.py
+++ jaxns/adaptive_refinement.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Tuple
+from typing import Tuple, Optional
 
 from etils.array_types import PRNGKey, FloatArray, IntArray, BoolArray
 from jax import core, numpy as jnp, tree_map, random, jit
@@ -8,7 +8,7 @@ from jax._src.lax.control_flow import while_loop
 from jaxns.internals.maps import chunked_pmap
 from jaxns.internals.stats import linear_to_log_stats
 from jaxns.model import Model
-from jaxns.slice_sampler import UniDimSliceSampler, SeedPoint, PreprocessType
+from jaxns.slice_sampler import UniDimSliceSampler, SeedPoint, PreprocessType, AbstractSliceSampler
 from jaxns.statistics import analyse_sample_collection
 from jaxns.types import NestedSamplerState, Reservoir, int_type
 from jaxns.utils import sort_samples
@@ -17,14 +17,17 @@ __all__ = ['AdaptiveRefinement']
 
 
 class AdaptiveRefinement:
-    def __init__(self, model: Model, uncert_improvement_patience: int, num_slices: int, num_parallel_samplers: int = 1):
+    def __init__(self, model: Model, uncert_improvement_patience: int, num_slices: int, num_parallel_samplers: int = 1,
+                 slice_sampler: Optional[AbstractSliceSampler] = None):
         if uncert_improvement_patience <= 0:
             raise ValueError(f"uncert_improvement_patient should be > 0, got {uncert_improvement_patience}.")
         self.uncert_improvement_patience = uncert_improvement_patience
         self.num_parallel_samplers = num_parallel_samplers
-        self.slice_sampler = UniDimSliceSampler(model=model,
-                                                midpoint_shrink=True,
-                                                multi_ellipse_bound=False)
+        if slice_sampler is None:
+            slice_sampler = UniDimSliceSampler(model=model,
+                                                    midpoint_shrink=True,
+                                                    multi_ellipse_bound=False)
+        self.slice_sampler = slice_sampler
         self.num_slices = num_slices
 
     def split_state(self, state: NestedSamplerState) -> Tuple[NestedSamplerState, Reservoir]:
