6613aeec3d3e07e61f916899b3ff79666137cb07
joshuaalbert <albert@strw.leidenuniv.nl>
2023-07-23 16:57:24 +0200

    * use chex instead of etils * add conjuction and disjunction of termination conditions

--- jaxns/termination.py
+++ jaxns/termination.py
@@ -1,30 +1,23 @@
-from typing import Optional
+from typing import Optional, Union
 
 from jax import numpy as jnp
 
 from jaxns.internals.log_semiring import LogSpace
 from jaxns.internals.stats import linear_to_log_stats, effective_sample_size
 from jaxns.types import TerminationCondition, SampleCollection, EvidenceCalculation, LivePoints, \
-    int_type
+    int_type, TerminationConditionConjunction, TerminationConditionDisjunction
 
 __all__ = ['determine_termination']
 
 
-def determine_termination(term_cond: TerminationCondition,
-                          sample_collection: Optional[SampleCollection] = None,
-                          evidence_calculation: Optional[EvidenceCalculation] = None,
-                          live_points: Optional[LivePoints] = None
-                          ):
+def determine_termination(
+        term_cond: Union[TerminationConditionDisjunction, TerminationConditionConjunction, TerminationCondition],
+        sample_collection: Optional[SampleCollection] = None,
+        evidence_calculation: Optional[EvidenceCalculation] = None,
+        live_points: Optional[LivePoints] = None
+):
     """
-    Determine if termination should happen.
-
-    Args:
-        term_cond: termination condition
-
-    Returns:
-        boolean done signal, and termination reason
-
-    Termination Flags are bits:
+    Determine if termination should happen. Termination Flags are bits:
         0-bit -> 1: used maximum allowed number of samples
         1-bit -> 2: evidence uncert below threshold
         2-bit -> 4: live points evidence below threshold
@@ -35,16 +28,39 @@ def determine_termination(term_cond: TerminationCondition,
         7-bit -> 128: entire live-points set is a single plateau
 
     Multiple flags are summed together
-    """
-    termination_condition = jnp.asarray(0, int_type)
-    done = jnp.asarray(False)
 
-    def _set_done_bit(bit, bit_reason, done, termination_condition):
-        done = done | bit
-        termination_condition += jnp.where(bit,
-                                           jnp.asarray(2 ** bit_reason, int_type),
-                                           jnp.asarray(0, int_type))
-        return done, termination_condition
+    Args:
+        term_cond: termination condition
+
+    Returns:
+        boolean done signal, and termination reason
+    """
+
+    termination_reason = jnp.asarray(0, int_type)
+    done = jnp.asarray(False, jnp.bool_)
+
+    def _set_done_bit(bit_done, bit_reason, done, termination_reason):
+        done = jnp.bitwise_or(bit_done, done)
+        termination_reason += jnp.where(bit_done,
+                                        jnp.asarray(2 ** bit_reason, int_type),
+                                        jnp.asarray(0, int_type))
+        return done, termination_reason
+
+    if isinstance(term_cond, TerminationConditionConjunction):
+        for c in term_cond.conds:
+            _done, _reason = determine_termination(term_cond=c, sample_collection=sample_collection,
+                                                   evidence_calculation=evidence_calculation, live_points=live_points)
+            done = jnp.bitwise_and(_done, done)
+            termination_reason = jnp.bitwise_and(_reason, termination_reason)
+        return done, termination_reason
+
+    if isinstance(term_cond, TerminationConditionDisjunction):
+        for c in term_cond.conds:
+            _done, _reason = determine_termination(term_cond=c, sample_collection=sample_collection,
+                                                   evidence_calculation=evidence_calculation, live_points=live_points)
+            done = jnp.bitwise_or(_done, done)
+            termination_reason = jnp.bitwise_or(_reason, termination_reason)
+        return done, termination_reason
 
     if term_cond.max_samples is not None:
         if sample_collection is None:
@@ -52,8 +68,8 @@ def determine_termination(term_cond: TerminationCondition,
         # used all points
         num_samples = sample_collection.sample_idx
         reached_max_samples = num_samples >= term_cond.max_samples
-        done, termination_condition = _set_done_bit(reached_max_samples, 0,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(reached_max_samples, 0,
+                                                 done=done, termination_reason=termination_reason)
     if term_cond.evidence_uncert is not None:
         if evidence_calculation is None:
             raise ValueError("evidence_calculation must not be None.")
@@ -61,8 +77,8 @@ def determine_termination(term_cond: TerminationCondition,
             log_f_mean=evidence_calculation.log_Z_mean,
             log_f2_mean=evidence_calculation.log_Z2_mean)
         evidence_uncert_low_enough = log_Z_var <= jnp.square(term_cond.evidence_uncert)
-        done, termination_condition = _set_done_bit(evidence_uncert_low_enough, 1,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(evidence_uncert_low_enough, 1,
+                                                 done=done, termination_reason=termination_reason)
     if term_cond.live_evidence_frac is not None:
         if evidence_calculation is None:
             raise ValueError("evidence_calculation must not be None.")
@@ -77,8 +93,8 @@ def determine_termination(term_cond: TerminationCondition,
         log_Z_upper = evidence_upper_bound.log_abs_val
 
         small_remaining_evidence = log_Z_remaining_upper - log_Z_upper < jnp.log(term_cond.live_evidence_frac)
-        done, termination_condition = _set_done_bit(small_remaining_evidence, 2,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(small_remaining_evidence, 2,
+                                                 done=done, termination_reason=termination_reason)
     if term_cond.ess is not None:
         if evidence_calculation is None:
             raise ValueError("evidence_calculation must not be None.")
@@ -86,15 +102,15 @@ def determine_termination(term_cond: TerminationCondition,
         ess = effective_sample_size(evidence_calculation.log_Z_mean,
                                     evidence_calculation.log_dZ2_mean)
         ess_reached = ess >= term_cond.ess
-        done, termination_condition = _set_done_bit(ess_reached, 3,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(ess_reached, 3,
+                                                 done=done, termination_reason=termination_reason)
     if term_cond.max_num_likelihood_evaluations is not None:
         if sample_collection is None:
             raise ValueError("sample_collections must not be None.")
         num_likelihood_evaluations = jnp.sum(sample_collection.reservoir.num_likelihood_evaluations)
         too_max_likelihood_evaluations = num_likelihood_evaluations >= term_cond.max_num_likelihood_evaluations
-        done, termination_condition = _set_done_bit(too_max_likelihood_evaluations, 4,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(too_max_likelihood_evaluations, 4,
+                                                 done=done, termination_reason=termination_reason)
 
     if term_cond.log_L_contour is not None:
         if sample_collection is None:
@@ -106,19 +122,19 @@ def determine_termination(term_cond: TerminationCondition,
                       )
         )
         likeihood_contour_reached = log_L_max >= term_cond.log_L_contour
-        done, termination_condition = _set_done_bit(likeihood_contour_reached, 5,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(likeihood_contour_reached, 5,
+                                                 done=done, termination_reason=termination_reason)
 
     if term_cond.efficiency_threshold is not None:
         if live_points is None:
             raise ValueError("live_points must be provided.")
         efficiency = jnp.reciprocal(jnp.mean(live_points.reservoir.num_likelihood_evaluations))
         efficiency_too_low = efficiency <= term_cond.efficiency_threshold
-        done, termination_condition = _set_done_bit(efficiency_too_low, 6,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(efficiency_too_low, 6,
+                                                 done=done, termination_reason=termination_reason)
     if (live_points is not None) and (live_points.reservoir.log_L.size > 1):
         all_plateau = jnp.min(live_points.reservoir.log_L) == jnp.max(live_points.reservoir.log_L)
-        done, termination_condition = _set_done_bit(all_plateau, 7,
-                                                    done=done, termination_condition=termination_condition)
+        done, termination_reason = _set_done_bit(all_plateau, 7,
+                                                 done=done, termination_reason=termination_reason)
 
-    return done, termination_condition
+    return done, termination_reason
