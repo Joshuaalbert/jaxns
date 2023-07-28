3afefbde50fe08f62b6d7c710729b64c845f929e
joshuaalbert <albert@strw.leidenuniv.nl>
2023-07-28 12:47:35 -0400

    * bump to 2.2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='jaxns',
-      version='2.2.1',
+      version='2.2.2',
       description='Nested Sampling in JAX',
       long_description=long_description,
       long_description_content_type="text/markdown",
