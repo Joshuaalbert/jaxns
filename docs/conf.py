30f630c05d84367c75f66d301b74eda572d2df51
joshuaalbert <albert@strw.leidenuniv.nl>
2023-12-18 17:52:34 +0100

    * Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sphinx_rtd_theme
 project = "jaxns"
 copyright = "2022, Joshua Albert"
 author = "Joshua Albert"
-release = "2.2.6"
+release = "2.3.0"
 
 
 # -- General configuration ---------------------------------------------------
