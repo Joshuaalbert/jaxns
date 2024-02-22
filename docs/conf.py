c1ce3fbf6a75e369352714bc7aa91e810a8ae5bd
joshuaalbert <albert@strw.leidenuniv.nl>
2024-02-21 22:28:47 -0500

    * Bump to 2.4.9

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sphinx_rtd_theme
 project = "jaxns"
 copyright = "2022, Joshua G. Albert"
 author = "Joshua G. Albert"
-release = "2.4.8"
+release = "2.4.9"
 
 
 # -- General configuration ---------------------------------------------------
