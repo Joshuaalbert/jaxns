7f8bff695cc29073cc4e5f3208511e53eabf821e
joshuaalbert <albert@strw.leidenuniv.nl>
2024-05-15 11:57:15 +0200

    * implement #59 * bump to 2.5.0

--- jaxns/framework/__init__.py
+++ jaxns/framework/__init__.py
@@ -1,4 +1,5 @@
 from jaxns.framework.model import *
 from jaxns.framework.prior import *
 from jaxns.framework.special_priors import *
+from jaxns.framework.jaxify import *
 from jaxns.framework.bases import PriorModelGen, PriorModelType
