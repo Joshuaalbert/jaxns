a4002e1926477189852725d7b1bbdfa4ff2fa151
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-03 13:08:08 +0100

    * Refactor into src folde * Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,41 +1,17 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages
 from setuptools import setup
 
-install_requires = [
-    'jax>=0.4.25',
-    'jaxlib',
-    'matplotlib',
-    'numpy<2',
-    'scipy',
-    'tensorflow_probability',
-    'tqdm',
-    'optax',
-    'jaxopt'
-]
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def load_requirements(file_name):
+    with open(file_name, "r") as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
 
-setup(name='jaxns',
-      version='2.6.2',
-      description='Nested Sampling in JAX',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/joshuaalbert/jaxns",
-      author='Joshua G. Albert',
-      author_email='albert@strw.leidenuniv.nl',
-      install_requires=install_requires,
-      tests_require=[
-          'pytest>=2.8',
-      ],
-      package_dir={'': './'},
-      packages=find_packages('./'),
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: Apache Software License",
-          "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.9',
-      )
+
+setup(
+    install_requires=load_requirements("requirements.txt"),
+    extras_require={
+        "examples": load_requirements("requirements-examples.txt"),
+    },
+    tests_require=load_requirements("requirements-tests.txt"),
+)
